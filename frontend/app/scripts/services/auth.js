ce42f872b1a71f46be7dd398f8cbba4838e1fd60
ruler88 <helloworld0424@gmail.com>
2015-01-18 22:33:34 -0800

    redirect after log-in 46

--- frontend/app/scripts/services/auth.js
+++ frontend/app/scripts/services/auth.js
@@ -8,16 +8,17 @@
  * Service in the jwtApp.
  */
 angular.module('jwtApp')
-  .service('auth', function ($http, API_URL, authToken) {
+  .service('auth', function ($http, API_URL, authToken, $state) {
 
     this.login = function(email, password) {
 			var url = API_URL + 'login';
 			return $http.post(url, {
 					email: email,
 					password: password
-				}).success(function() {
-				authToken.setToken(res.token);
-			});
+				}).success(function(res) {
+					authToken.setToken(res.token);
+					$state.go('main');
+				});
 		}
 
   });
